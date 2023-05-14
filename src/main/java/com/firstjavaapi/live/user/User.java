768056edb7cc66c758948e0b1c26e2043d4e7add
Abdulla al Rakib <abdullaalrakib@Abdullas-MacBook-Pro.local>
2023-05-14 16:34:41 +0200

    javaapifinished

---
+++ src/main/java/com/firstjavaapi/live/user/User.java
@@ -0,0 +1,44 @@
+package com.firstjavaapi.live.user;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+
+    @Column(name = "email")
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/com/firstjavaapi/live/user/UserRepository.java
@@ -0,0 +1,7 @@
+package com.firstjavaapi.live.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {}
